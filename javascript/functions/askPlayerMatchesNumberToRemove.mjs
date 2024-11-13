ed02eb4df213adb71ad250cb01c8986fbe4cd98b
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-13 16:20:51 +0100

    refactor: 💡  enhanced semantics

--- javascript/functions/askPlayerMatchesNumberToRemove.mjs
+++ javascript/functions/askPlayerMatchesNumberToRemove.mjs
@@ -3,7 +3,7 @@
 import gameStateVariables from "../variables/gameStateVariables.mjs";
 
 function askPlayerMatchesNumberToRemove() {
-  const userInput = prompt(
+  const playerInput = prompt(
     `It's ${
       gameStateVariables.numberOfRemovals % 2 === 0 ? "player 1" : "player 2"
     }'s turn. How many matches would ${
@@ -11,15 +11,15 @@ function askPlayerMatchesNumberToRemove() {
     } like to remove?`,
   );
   if (
-    isNaN(userInput) ||
-    userInput === "" ||
-    userInput === null ||
-    userInput === undefined
+    isNaN(playerInput) ||
+    playerInput === "" ||
+    playerInput === null ||
+    playerInput === undefined
   ) {
     console.log("Please enter a number.");
     return;
   }
-  const numberOfMatchesToRemove = parseInt(userInput);
+  const numberOfMatchesToRemove = parseInt(playerInput);
   if (numberOfMatchesToRemove < 1 || numberOfMatchesToRemove > 6) {
     console.log(
       "1 match minimum and 6 matches maximum can be removed at a time.",
