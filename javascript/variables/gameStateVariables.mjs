04e823de6abdb6ce41e6911bfc7ec9044d463059
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-13 15:45:19 +0100

    refactor: 💡  changed file extension

---
+++ javascript/variables/gameStateVariables.mjs
@@ -0,0 +1,9 @@
+// javascript\variables\gameStateVariables.mjs
+
+const gameStateVariables = {
+  numberOfMatchesRemaining: 50,
+  numberOfMatchesToRemove: 0,
+  numberOfRemovals: 0,
+};
+
+export default gameStateVariables;
