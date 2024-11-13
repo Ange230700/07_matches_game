f2b16c7c55253e16d494b45f202d6009018ceb9c
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-13 16:22:46 +0100

    feat: 🎸  implemented function asking the installer to set the players number

--- javascript/variables/gameStateVariables.mjs
+++ javascript/variables/gameStateVariables.mjs
@@ -1,6 +1,7 @@
 // javascript\variables\gameStateVariables.mjs
 
 const gameStateVariables = {
+  numberOfPlayers: 2,
   numberOfMatchesRemaining: 50,
   numberOfMatchesToRemove: 0,
   numberOfRemovals: 0,
