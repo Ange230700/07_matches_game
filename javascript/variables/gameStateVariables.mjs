c5166c9e81577030533776eef444896c330b5271
Ange <93579219+Ange230700@users.noreply.github.com>
2024-11-13 23:08:36 +0100

    Revert "refactor: 💡  changed file extension"
    This reverts commit 24b5a24f6d1cbecb6bcc67429fad03f10793cb75.

--- javascript/variables/gameStateVariables.mjs
+++
@@ -1,9 +0,0 @@
-// javascript\variables\gameStateVariables.mjs
-
-const gameStateVariables = {
-  numberOfMatchesRemaining: 50,
-  numberOfMatchesToRemove: 0,
-  numberOfRemovals: 0,
-};
-
-export default gameStateVariables;
