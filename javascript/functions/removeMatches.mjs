653656fd57ba7db500091491b9c550567da298db
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-13 14:16:39 +0100

    feat: 🎸  implemented user input

--- javascript/functions/removeMatches.mjs
+++ javascript/functions/removeMatches.mjs
@@ -1,20 +1,25 @@
 // javascript\functions\removeMatches.mjs
 
-import gameStateVariables from '../variables/gameStateVariables.js';
+import gameStateVariables from "../variables/gameStateVariables.js";
 
 function removeMatches(numberOfMatchesToRemove) {
-    if (numberOfMatchesToRemove < 1 || numberOfMatchesToRemove > 6) {
-        console.log("You can only remove between 1 and 6 matches.");
-        return;
-    }
-    if (gameStateVariables.numberOfMatchesRemaining - numberOfMatchesToRemove < 0) {
-        console.log('There are not enough matches to remove.');
-        return;
-    }
-    gameStateVariables.numberOfMatchesRemaining -= numberOfMatchesToRemove;
-    console.log(`You removed ${numberOfMatchesToRemove} matches.`);
-    console.log(`There are ${gameStateVariables.numberOfMatchesRemaining} matches remaining.`);
-    return gameStateVariables.numberOfMatchesRemaining;
+  if (numberOfMatchesToRemove < 1 || numberOfMatchesToRemove > 6) {
+    console.log("You can only remove between 1 and 6 matches.");
+    return;
+  }
+  if (
+    gameStateVariables.numberOfMatchesRemaining - numberOfMatchesToRemove <
+    0
+  ) {
+    console.log("There are not enough matches to remove.");
+    return;
+  }
+  gameStateVariables.numberOfMatchesRemaining -= numberOfMatchesToRemove;
+  console.log(`You removed ${numberOfMatchesToRemove} matches.`);
+  console.log(
+    `There are ${gameStateVariables.numberOfMatchesRemaining} matches remaining.`,
+  );
+  return gameStateVariables.numberOfMatchesRemaining;
 }
 
 export default removeMatches;
--- javascript/variables/gameStateVariables.js
+++ javascript/variables/gameStateVariables.js
@@ -1,8 +1,8 @@
 // javascript\variables\gameStateVariables.js
 
 const gameStateVariables = {
-    numberOfMatchesRemaining: 50,
-    numberOfMatchesToRemove: 0
+  numberOfMatchesRemaining: 50,
+  numberOfMatchesToRemove: 0,
 };
 
 export default gameStateVariables;
