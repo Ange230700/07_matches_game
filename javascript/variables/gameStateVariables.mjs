c446e5fb4a018ed36a0cb3a3974aa7101a43775c
Ange <93579219+Ange230700@users.noreply.github.com>
2024-11-13 23:08:36 +0100

    Revert "feat: 🎸  implemented function asking the installer to set the players number"
    This reverts commit fa9a7d9a3e943f98d8f3c6eb47d53727705f4f88.

--- javascript/variables/gameStateVariables.mjs
+++ javascript/variables/gameStateVariables.mjs
@@ -1,7 +1,6 @@
 // javascript\variables\gameStateVariables.mjs
 
 const gameStateVariables = {
-  numberOfPlayers: 2,
   numberOfMatchesRemaining: 50,
   numberOfMatchesToRemove: 0,
   numberOfRemovals: 0,
