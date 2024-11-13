f4f63c09e3b06b7cadbe31c28f82df2c7401ae3f
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-13 15:50:16 +0100

    feat: 🎸  implemented the game logic

--- javascript/variables/gameStateVariables.js
+++
@@ -1,9 +0,0 @@
-// javascript\variables\gameStateVariables.js
-
-const gameStateVariables = {
-  numberOfMatchesRemaining: 50,
-  numberOfMatchesToRemove: 0,
-  numberOfRemovals: 0,
-};
-
-export default gameStateVariables;
