f3269eecfadc6bd3f2a4fec1149ac2d35b1fcaf3
Ange <93579219+Ange230700@users.noreply.github.com>
2024-11-13 23:08:36 +0100

    Revert "feat: 🎸  implemented the game logic"
    This reverts commit 15eb724a2db8b4e067c2a9b9365bb96bb2c99c57.

---
+++ javascript/variables/gameStateVariables.js
@@ -0,0 +1,9 @@
+// javascript\variables\gameStateVariables.js
+
+const gameStateVariables = {
+  numberOfMatchesRemaining: 50,
+  numberOfMatchesToRemove: 0,
+  numberOfRemovals: 0,
+};
+
+export default gameStateVariables;
