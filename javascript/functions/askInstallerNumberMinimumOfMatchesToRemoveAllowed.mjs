ae4053d4293db5dfc48ab4a069d47b802b3bcfe6
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-13 23:06:00 +0100

    feat: 🎸  upgraded to multi-player mode

--- javascript/functions/askInstallerNumberMinimumOfMatchesToRemoveAllowed.mjs
+++ javascript/functions/askInstallerNumberMinimumOfMatchesToRemoveAllowed.mjs
@@ -3,25 +3,26 @@
 import gameStateVariables from "../variables/gameStateVariables.mjs";
 
 function askInstallerNumberMinimumOfMatchesToRemoveAllowed() {
-    const installerInput = prompt(
-        "What is the minimum number of matches that can be removed at a time?",
-    );
-    if (
-        isNaN(installerInput) ||
-        installerInput === "" ||
-        installerInput === null ||
-        installerInput === undefined
-    ) {
-        console.log("Please enter a number.");
-        return;
-    }
-    const numberMinimumOfMatchesToRemoveAllowed = parseInt(installerInput);
-    if (numberMinimumOfMatchesToRemoveAllowed < 1) {
-        console.log("1 match minimum can be removed at a time.");
-        return;
-    }
-    gameStateVariables.numberMinimumOfMatchesToRemoveAllowed = numberMinimumOfMatchesToRemoveAllowed;
-    return numberMinimumOfMatchesToRemoveAllowed;
+  const installerInput = prompt(
+    "What is the minimum number of matches that can be removed at a time?",
+  );
+  if (
+    isNaN(installerInput) ||
+    installerInput === "" ||
+    installerInput === null ||
+    installerInput === undefined
+  ) {
+    console.log("Please enter a number.");
+    return;
+  }
+  const numberMinimumOfMatchesToRemoveAllowed = parseInt(installerInput);
+  if (numberMinimumOfMatchesToRemoveAllowed < 1) {
+    console.log("1 match minimum can be removed at a time.");
+    return;
+  }
+  gameStateVariables.numberMinimumOfMatchesToRemoveAllowed =
+    numberMinimumOfMatchesToRemoveAllowed;
+  return numberMinimumOfMatchesToRemoveAllowed;
 }
 
 export default askInstallerNumberMinimumOfMatchesToRemoveAllowed;
--- javascript/functions/askPlayerMatchesNumberToRemove.mjs
+++ javascript/functions/askPlayerMatchesNumberToRemove.mjs
@@ -3,12 +3,11 @@
 import gameStateVariables from "../variables/gameStateVariables.mjs";
 
 function askPlayerMatchesNumberToRemove() {
+  const currentPlayerNumber =
+    (gameStateVariables.numberOfRemovals % gameStateVariables.numberOfPlayers) +
+    1;
   const playerInput = prompt(
-    `It's ${
-      gameStateVariables.numberOfRemovals % 2 === 0 ? "player 1" : "player 2"
-    }'s turn. How many matches would ${
-      gameStateVariables.numberOfRemovals % 2 === 0 ? "player 1" : "player 2"
-    } like to remove?`,
+    `It's player ${currentPlayerNumber}'s turn. How many matches would player ${currentPlayerNumber} like to remove?`,
   );
   if (
     isNaN(playerInput) ||
@@ -20,9 +19,14 @@ function askPlayerMatchesNumberToRemove() {
     return;
   }
   const numberOfMatchesToRemove = parseInt(playerInput);
-  if (numberOfMatchesToRemove < 1 || numberOfMatchesToRemove > 6) {
+  if (
+    numberOfMatchesToRemove <
+      gameStateVariables.numberMinimumOfMatchesToRemoveAllowed ||
+    numberOfMatchesToRemove >
+      gameStateVariables.numberMaximumOfMatchesToRemoveAllowed
+  ) {
     console.log(
-      "1 match minimum and 6 matches maximum can be removed at a time.",
+      `${gameStateVariables.numberMinimumOfMatchesToRemoveAllowed} match(es) minimum and ${gameStateVariables.numberMaximumOfMatchesToRemoveAllowed} matches maximum can be removed at a time.`,
     );
     return;
   }
