b5f20c2ff505171888f3ac118dd53e09b3fb41ad
Ange <93579219+Ange230700@users.noreply.github.com>
2024-11-13 23:08:36 +0100

    Revert "refactor: 💡  enhanced semantics"
    This reverts commit 494361e7628f0033c9724f4e8769ef0696a22300.

--- javascript/functions/askPlayerMatchesNumberToRemove.mjs
+++ javascript/functions/askPlayerMatchesNumberToRemove.mjs
@@ -3,7 +3,7 @@
 import gameStateVariables from "../variables/gameStateVariables.mjs";
 
 function askPlayerMatchesNumberToRemove() {
-  const playerInput = prompt(
+  const userInput = prompt(
     `It's ${
       gameStateVariables.numberOfRemovals % 2 === 0 ? "player 1" : "player 2"
     }'s turn. How many matches would ${
@@ -11,15 +11,15 @@ function askPlayerMatchesNumberToRemove() {
     } like to remove?`,
   );
   if (
-    isNaN(playerInput) ||
-    playerInput === "" ||
-    playerInput === null ||
-    playerInput === undefined
+    isNaN(userInput) ||
+    userInput === "" ||
+    userInput === null ||
+    userInput === undefined
   ) {
     console.log("Please enter a number.");
     return;
   }
-  const numberOfMatchesToRemove = parseInt(playerInput);
+  const numberOfMatchesToRemove = parseInt(userInput);
   if (numberOfMatchesToRemove < 1 || numberOfMatchesToRemove > 6) {
     console.log(
       "1 match minimum and 6 matches maximum can be removed at a time.",
