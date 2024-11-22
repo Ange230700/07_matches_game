fbc72643e0509ab8156922f7b592fdaf3e0824fa
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-22 11:59:50 +0100

    refactor(whole game logic): 💡  modularized gameplay

--- javascript/functions/playMatchesGame.mjs
+++ javascript/functions/playMatchesGame.mjs
@@ -1,41 +1,51 @@
 // javascript\functions\playMatchesGame.mjs
 
-import setUpMatchesGame from "./setUpMatchesGame.mjs";
 import gameStateVariables from "../variables/gameStateVariables.mjs";
-import askPlayerMatchesNumberToRemove from "./askPlayerMatchesNumberToRemove.mjs";
-import removeMatches from "./removeMatches.mjs";
-import hasPlayerWon from "./hasPlayerWon.mjs";
+import setUpMatchesGame from "./setUpMatchesGame.mjs";
+import getPlayerInput from "./getPlayerInput.mjs";
+import updateGameState from "./updateGameState.mjs";
+import {hasPlayerWon, isItDraw} from "./endGameValidation.mjs";
+import switchPlayer from "./switchPlayer.mjs";
 
 function playMatchesGame() {
   alert("Welcome to the Matches Game!");
 
-  alert("Let the installer set up the game.");
+  if (gameStateVariables.isSetupComplete === false) {
+    const setupResult = setUpMatchesGame();
 
-  while (setUpMatchesGame() === false) {
-    alert(
-      "The game setup is not complete. Please make sure the installer sets up the game properly.",
-    );
-  }
-
-  alert("The settings are properly done. Let's play the game!");
-
-  let numberOfMatchesToRemove = askPlayerMatchesNumberToRemove();
-
-  removeMatches(numberOfMatchesToRemove);
-
-  if (hasPlayerWon()) {
-    return;
-  }
-
-  while (gameStateVariables.numberOfMatchesRemaining > 0) {
-    numberOfMatchesToRemove = askPlayerMatchesNumberToRemove();
-
-    removeMatches(numberOfMatchesToRemove);
-
-    if (hasPlayerWon()) {
+    if (setupResult === undefined) {
       return;
     }
   }
+
+  alert("Let's play the game, shall we?");
+
+  let numberOfMatchesToRemove;
+
+  while (gameStateVariables.numberOfMatchesRemaining > 0) {
+    numberOfMatchesToRemove = getPlayerInput();
+    if (numberOfMatchesToRemove === undefined) {
+      return;
+    }
+
+    updateGameState(numberOfMatchesToRemove);
+
+    if (hasPlayerWon() === true) {
+      alert(`Player ${gameStateVariables.currentPlayerNumber} wins!`);
+
+      return;
+    }
+
+    if (isItDraw() === true) {
+      alert("There are not enough match(es) to continue the game! It's a draw!");
+
+      return;
+    }
+
+    switchPlayer();
+  }
+
+  return;
 }
 
 export default playMatchesGame;
