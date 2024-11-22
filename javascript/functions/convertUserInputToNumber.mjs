b468f63bd28da6df63b6d033325c7af3d8e82848
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-22 12:01:11 +0100

    refactor: 💡  deleted stuff no longer needed

--- javascript/functions/convertUserInputToNumber.mjs
+++
@@ -1,9 +0,0 @@
-// javascript\functions\convertUserInputToNumber.mjs
-
-function convertUserInputToNumber(userInput) {
-  const convertedUserInput = parseInt(userInput, 10);
-
-  return convertedUserInput;
-}
-
-export default convertUserInputToNumber;
